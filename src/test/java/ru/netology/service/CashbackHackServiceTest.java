463d1477e932bda6017b3b72f1698f51077d840b
yourkeksuk <igor-18css@mail.ru>
2022-09-25 17:38:57 +0300

    Test commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -21,7 +21,7 @@ public class CashbackHackServiceTest {
     public void moreBoundarySum() {
         CashbackHackService service = new CashbackHackService();
 
-        int amount = 10200;
+        int amount = 1000;
         int expected = 1000;
         int actual = service.remain(amount);
         Assert.assertEquals(actual, expected);
