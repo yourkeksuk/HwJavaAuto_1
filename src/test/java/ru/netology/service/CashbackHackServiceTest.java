9c723f6ea1f9ae2a9c0f6fc8caf64377bafd6ed5
yourkeksuk <igor-18css@mail.ru>
2022-09-25 18:14:34 +0300

    Test commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,22 +7,22 @@ import org.testng.annotations.Test;
 public class CashbackHackServiceTest {
 
     @Test
-    public void testRemain() {
+    public void remain() {
         CashbackHackService service = new CashbackHackService();
 
         int amount = 900;
-        int expected = 0;
+        int expected = 100;
         int actual = service.remain(amount);
         Assert.assertEquals(actual, expected);
         System.out.println("Кэшбэк начисляется при заказе на сумму от 1000р. Дополните Ваш заказ и получите кэшбэк!");
     }
 
     @Test
-    public void moreBoundarySum() {
+    public void equalBoundary() {
         CashbackHackService service = new CashbackHackService();
 
         int amount = 1000;
-        int expected = 1000;
+        int expected = 0;
         int actual = service.remain(amount);
         Assert.assertEquals(actual, expected);
     }
